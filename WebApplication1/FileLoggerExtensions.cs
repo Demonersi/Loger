23cb1bc8fc6b091386c4317b061eb8c82ec2e2d5
student <student@Y5-13>
2023-01-09 18:51:12 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/FileLoggerExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string filePath)
+        {
+            builder.AddProvider(new FileLoggerProvider(filePath));
+            return builder;
+        }
+    }
+}
